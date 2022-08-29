bf56d50e9ec1e930488282ad062f180c18df4276
Mishu Rahman <mishu.cse616@gmail.com>
2022-08-29 09:19:22 +0600

    useEffect hook, localStorage
    How use useEffect and localStorage

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,33 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Header from './Header';
 import AddContact from './AddContact';
 import ContactList from './ContactList';
 
 function App() {
+  const LOCAL_STORAGE_KEY="contacts";
 const [contacts, setContact] = useState([]);
+
 const addContactHandler = (contact)=>{
   setContact([...contacts, contact]);
 }
+
+useEffect(()=>{
+  const retrivedContacts=JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    console.log(retrivedContacts); 
+  if(retrivedContacts) {
+      setContact(retrivedContacts)
+    };
+}, []);
+
+  useEffect(()=>{
+    if(contacts.length > 0){
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+    }
+  }, [contacts]);
+  
+
+
   return (
     <div className='ui container'>
       <Header />
